7c9404859d1d0f87e0e5712fcd078b8d0aa2037c
Sanwal Khan <71782878+sanwalkhan@users.noreply.github.com>
2021-02-05 01:05:45 +0500

    Add files via upload

---
+++ Website/main.js
@@ -0,0 +1,13 @@
+burger = document.querySelector(".burger")
+
+navbar = document.querySelector(".navbar")
+navList = document.querySelector(".nav-list")
+
+
+burger.addEventListener("click", ()=>
+{
+
+    navbar.classList.toggle("h-nav");
+    navList.classList.toggle("v-class");
+    
+})
